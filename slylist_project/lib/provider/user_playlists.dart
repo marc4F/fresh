c2895732f8d561bb97f98449edc0b24f823c0fb7
Marco Martini <marco.martini@zi.uzh.ch>
2020-05-11 13:00:12 +0200

    -

--- slylist_project/lib/provider/user_playlists.dart
+++ slylist_project/lib/provider/user_playlists.dart
@@ -7,7 +7,7 @@ class UserPlaylistsProvider extends ChangeNotifier {
   void addUserPlaylist() {
     Playlist pl1 = Playlist();
     Playlist pl2 = Playlist();
-    pl1.name = "Neue Lieblingstitel2";
+    pl1.name = "Neue Lieblingstitel3";
     pl2.name = "Topaktuell";
     _playlists.add(pl1);
     _playlists.add(pl2);
